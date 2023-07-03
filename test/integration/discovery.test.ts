c506bfbf98f03242cfcfa93f2bea3ddde44e0b52
José Luis Landabaso Díaz <landabaso@gmail.com>
2023-07-03 10:47:40 +0200

    Refactor fixtures & tests to ressemble discoveryInfo

--- test/integration/discovery.test.ts
+++ test/integration/discovery.test.ts
@@ -2,6 +2,7 @@
 // Distributed under the MIT software license
 
 //TODO: TEST duplicates of scriptPubKey... Imagine expressions using uppercase/lowecase of the same or equivalent ones using xpub and wif for the same scriptPubKey for example...
+//TEST: functions keep same reference
 //TODO: Test that The sorting is the same for esplora and electrum with older
 //txs at the beginning of the array and mempool ones at the end
 //TODO: Test error if getUtxos without first discoverTxs or discover
@@ -10,116 +11,17 @@
 //Some electrum servers:
 //https://1209k.com/bitcoin-eye/ele.php
 
-import { RegtestUtils } from 'regtest-client';
 import { networks } from 'bitcoinjs-lib';
 import * as secp256k1 from '@bitcoinerlab/secp256k1';
 import * as descriptors from '@bitcoinerlab/descriptors';
 import { mnemonicToSeedSync } from 'bip39';
-import {
-  EsploraExplorer,
-  ElectrumExplorer,
-  ESPLORA_LOCAL_REGTEST_URL
-} from '@bitcoinerlab/explorer';
-const { Descriptor, BIP32 } = descriptors.DescriptorsFactory(secp256k1);
-const regtestUtils = new RegtestUtils();
+import { ElectrumExplorer } from '@bitcoinerlab/explorer';
+const { BIP32 } = descriptors.DescriptorsFactory(secp256k1);
 import { DiscoveryFactory, TxStatus, Wallet } from '../../dist';
-
-import { fixtures } from '../fixtures/discovery';
-const network = networks.regtest;
-
-const regtestTest = false;
 
 const onWalletUsed = (wallet: Wallet) => {
   console.log(`TRACE - onWalletUsed(${wallet}`);
 };
-
-if (regtestTest) {
-  const explorer = new EsploraExplorer({
-    url: ESPLORA_LOCAL_REGTEST_URL,
-    irrevConfThresh: 3,
-    maxTxPerScriptPubKey: 1000
-  });
-  const { Discovery } = DiscoveryFactory(explorer);
-  describe('Discovery on regtest', () => {
-    test(`Fund`, async () => {
-      //Let's fund (if needed fund=true) && test (fund=false) the descriptors:
-      for (const funding of [true, false]) {
-        for (const descriptor of fixtures.regtest.descriptors) {
-          if (descriptor.expression.indexOf('*') !== -1) {
-            for (const key in descriptor.funds) {
-              const index = parseInt(key, 10);
-              const address = new Descriptor({
-                expression: descriptor.expression,
-                network,
-                index
-              }).getAddress();
-              const value = descriptor.funds[index]!;
-              const { balance } = await explorer.fetchAddress(address);
-              if (funding) {
-                //Only fund it if this file has not been run already
-                if (balance === 0) await regtestUtils.faucet(address, value);
-              } else {
-                expect(balance).toEqual(value);
-              }
-            }
-          } else if (typeof descriptor.value !== 'undefined') {
-            //This is a non-ranged descriptor
-            const address = new Descriptor({
-              expression: descriptor.expression,
-              network
-            }).getAddress();
-            const value = descriptor.value;
-            const { balance } = await explorer.fetchAddress(address);
-            if (funding) {
-              //Only fund it if this file has not been run already
-              if (balance === 0) await regtestUtils.faucet(address, value);
-            } else {
-              expect(balance).toEqual(value);
-            }
-          }
-        }
-        //Confirm the transactions above
-        if (funding) {
-          await regtestUtils.mine(6);
-          await new Promise(resolve => setTimeout(resolve, 5000)); //sleep 5 sec
-        }
-      }
-    }, 20000);
-
-    test(
-      `Discover on regtest`,
-      async () => {
-        const discovery = new Discovery();
-        for (const descriptor of fixtures.regtest.descriptors) {
-          await discovery.discover({
-            //gapLimit: 1, //TODO: TEST THIS. with gapLimit 1 it should not get anything
-            expressions: descriptor.expression,
-            network
-          });
-        }
-        const masterNode = BIP32.fromSeed(
-          mnemonicToSeedSync(fixtures.regtest.mnemonic),
-          network
-        );
-        await discovery.discoverStandardWallets({
-          masterNode,
-          network,
-          onWalletUsed
-        });
-        //console.log(
-        //  JSON.stringify(
-        //    discovery.getWallets({ network: networks.regtest }),
-        //    null,
-        //    2
-        //  )
-        //);
-        //await discovery.discoverTxs({ network });
-        console.log(JSON.stringify(discovery.getDiscoveryInfo(), null, 2));
-      },
-      60 * 5 * 1000
-    );
-  });
-}
 
 console.log(ElectrumExplorer);
 for (const network of [networks.bitcoin]) {
---
+++ test/integration/regtest.test.ts
@@ -0,0 +1,107 @@
+// Copyright (c) 2023 Jose-Luis Landabaso - https://bitcoinerlab.com
+// Distributed under the MIT software license
+
+import { RegtestUtils } from 'regtest-client';
+import { networks } from 'bitcoinjs-lib';
+import * as secp256k1 from '@bitcoinerlab/secp256k1';
+import * as descriptors from '@bitcoinerlab/descriptors';
+import { mnemonicToSeedSync } from 'bip39';
+import {
+  EsploraExplorer,
+  ElectrumExplorer,
+  ESPLORA_LOCAL_REGTEST_URL
+} from '@bitcoinerlab/explorer';
+const { Descriptor, BIP32 } = descriptors.DescriptorsFactory(secp256k1);
+const regtestUtils = new RegtestUtils();
+import { DiscoveryFactory, Wallet } from '../../dist';
+
+import { fixtures } from '../fixtures/discovery';
+const network = networks.regtest;
+
+const onWalletUsed = (wallet: Wallet) => {
+  console.log(`TRACE - onWalletUsed(${wallet}`);
+};
+
+describe('Discovery on regtest', () => {
+  for (const explorer of [
+    {
+      name: 'Esplora',
+      instance: new EsploraExplorer({
+        url: ESPLORA_LOCAL_REGTEST_URL,
+        irrevConfThresh: 3,
+        maxTxPerScriptPubKey: 1000
+      })
+    },
+    {
+      name: 'Electrum',
+      instance: new ElectrumExplorer({
+        network,
+        irrevConfThresh: 3,
+        maxTxPerScriptPubKey: 1000
+      })
+    }
+  ]) {
+    const { Discovery } = DiscoveryFactory(explorer.instance);
+    test(`Fund with ${explorer.name}`, async () => {
+      //Let's fund (if needed fund=true) && test (fund=false) the descriptors:
+      for (const funding of [true, false]) {
+        for (const [expression, scriptPubKeys] of Object.entries(
+          fixtures.regtest.descriptors
+        )) {
+          for (const [index, value] of Object.entries(scriptPubKeys)) {
+            const address = new Descriptor({
+              expression,
+              network,
+              ...(index === 'non-ranged' ? {} : { index: Number(index) })
+            }).getAddress();
+            const { balance } = await explorer.instance.fetchAddress(address);
+            if (funding) {
+              //Fund it only when not been founded already (in previous test runs)
+              if (balance === 0) await regtestUtils.faucet(address, value);
+            } else {
+              expect(balance).toEqual(value);
+            }
+          }
+        }
+        //Confirm the transactions above
+        if (funding) {
+          await regtestUtils.mine(6);
+          await new Promise(resolve => setTimeout(resolve, 5000)); //sleep 5 sec
+        }
+      }
+    }, 20000);
+
+    test(
+      `Discover on regtest`,
+      async () => {
+        const discovery = new Discovery();
+        for (const expression of Object.keys(fixtures.regtest.descriptors)) {
+          await discovery.discover({
+            //gapLimit: 1, //TODO: TEST THIS. with gapLimit 1 it should not get anything
+            expressions: expression,
+            network
+          });
+        }
+        const masterNode = BIP32.fromSeed(
+          mnemonicToSeedSync(fixtures.regtest.mnemonic),
+          network
+        );
+        await discovery.discoverStandardWallets({
+          masterNode,
+          network,
+          onWalletUsed
+        });
+        //console.log(
+        //  JSON.stringify(
+        //    discovery.getWallets({ network: networks.regtest }),
+        //    null,
+        //    2
+        //  )
+        //);
+        //await discovery.discoverTxs({ network });
+        console.log(JSON.stringify(discovery.getDiscoveryInfo(), null, 2));
+      },
+      60 * 5 * 1000
+    );
+  }
+});

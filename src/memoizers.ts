5da08cdcccba5a10c5eebdeca82ca05d33895fec
José Luis Landabaso Díaz <landabaso@gmail.com>
2023-07-03 16:55:45 +0200

    Fix memoizeOneWithShallowArraysCheck - it now properly evaluates the original function on arguments change

--- src/memoizers.ts
+++ src/memoizers.ts
@@ -1,4 +1,3 @@
-import memoizee from 'memoizee';
 import { shallowEqualArrays } from 'shallow-equal';
 
 /**
@@ -21,60 +20,26 @@ export function memoizeOneWithShallowArraysCheck<
   T extends unknown[],
   R extends unknown[]
 >(func: (...args: T) => R) {
+  let lastArgs: T | null = null;
   let lastResult: R | null = null;
 
-  return memoizee(
-    (...args: T) => {
-      const newResult = func(...args);
+  return (...args: T): R => {
+    // If the new args are shallowly equal to the last args, return the last result
+    if (lastArgs && lastResult && shallowEqualArrays(lastArgs, args)) {
+      return lastResult;
+    }
+    lastArgs = args;
 
-      if (lastResult && shallowEqualArrays(lastResult, newResult)) {
-        return lastResult;
-      }
+    const newResult = func(...args);
 
+    if (lastResult && shallowEqualArrays(lastResult, newResult)) {
+      // If the new result is shallowly equal to the last result, return the last result
+      return lastResult;
+    } else {
       lastResult = newResult;
+
+      // Return the new result
       return newResult;
-    },
-    { max: 1 }
-  );
-}
-
-/**
- * This function is an extension of memoizee which stores the result of the latest call (cache size one).
- * If the arguments for the current call are the same as the latest call, it will return the same result.
- * If the arguments are different, but the returned Object or Array is deeply equal to the previous one, it still returns the same object.
- * Note: This uses JSON.stringify for deep comparisons which might not be suitable for large objects or arrays.
- *
- * @template T - The type of input arguments to the function to be memoized.
- * @template R - The type of the return value of the function to be memoized.
- * @param {(...args: T) => R} func - The function to be memoized.
- * @returns {(...args: T) => R} A memoized version of the input function.
- *
- * @example
- * const memoizedFunc = memoizeOneWithDeepCheck(myFunc);
- * const result1 = memoizedFunc(arg1, arg2);
- * const result2 = memoizedFunc(arg1, arg2); // Will return the same object as result1
- * const result3 = memoizedFunc(arg3, arg4); // If the result is deeply equal to result1, it will still return the same object as result1
- */
-export function memoizeOneWithDeepCheck<
-  T extends unknown[],
-  R extends unknown[]
->(func: (...args: T) => R) {
-  let lastResult: R | null = null;
-
-  return memoizee(
-    (...args: T) => {
-      const newResult = func(...args);
-
-      if (
-        lastResult &&
-        JSON.stringify(lastResult) === JSON.stringify(newResult)
-      ) {
-        return lastResult;
-      }
-
-      lastResult = newResult;
-      return newResult;
-    },
-    { max: 1 }
-  );
+    }
+  };
 }
